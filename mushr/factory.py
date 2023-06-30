11f0ef25cd334f2e72f54c6279a30a18fe9925a8
Aravind Sivaramakrishnan <aravind.siva@rutgers.edu>
2023-06-30 15:00:37 -0400

    Re-organize and add MuSHR environments

---
+++ mushr/factory.py
@@ -0,0 +1,17 @@
+from env import MushrReachEnv
+from gym.envs.registration import register
+
+class MushrEnvironmentFactory:
+    def __init__(self):
+        self.noise_levels = [0.01, 0.05, 0.1]
+        self.noise_level_names = ["Low", "Med", "High"]
+    
+    def register_environments_with_position_goals(self):
+        register(id="MushrObsEnv-v0",entry_point=MushrReachEnv, kwargs={'noisy': False, 'use_obs': True, 'use_orientation': False})
+        for i, noise_level in enumerate(self.noise_levels):
+            register(id="MushrObs"+self.noise_level_names[i]+"NoisyEnv-v0",entry_point=MushrReachEnv, kwargs={'noisy': True, 'use_obs': True, 'use_orientation': False, 'noise_scale': noise_level})
+        
+    def register_environments_with_position_and_orientation_goals(self):
+        register(id="MushrObsEnv-v0",entry_point=MushrReachEnv, kwargs={'noisy': False, 'use_obs': True, 'use_orientation': True})
+        for i, noise_level in enumerate(self.noise_levels):
+            register(id="MushrObs"+self.noise_level_names[i]+"NoisyEnv-v0",entry_point=MushrReachEnv, kwargs={'noisy': True, 'use_obs': True, 'use_orientation': True, 'noise_scale': noise_level})
---
+++ mushr/train.py
@@ -0,0 +1,45 @@
+from factory import MushrEnvironmentFactory
+env_factory = MushrEnvironmentFactory()
+env_factory.register_environments_with_position_and_orientation_goals()
+
+from stable_baselines3 import HER, SAC
+import gym
+import os
+import numpy as np 
+np.set_printoptions(suppress=True)
+import matplotlib.pyplot as plt
+
+env_name = "MushrObs"
+
+env = gym.make(env_name+"Env-v0")
+alg = SAC
+num_steps = int(5e5)
+
+model = HER('MlpPolicy', env, alg, n_sampled_goal=4,
+            goal_selection_strategy='future',
+            verbose=1, buffer_size=int(1e6),
+            learning_rate=1e-3,
+            gamma=0.95, batch_size=256,max_episode_length=30)
+
+from stable_baselines3.common.callbacks import EvalCallback, CheckpointCallback, CallbackList
+from stable_baselines3.common.vec_env import DummyVecEnv
+from stable_baselines3.her import ObsDictWrapper
+
+train_model = True
+
+eval_env = DummyVecEnv([lambda: gym.make(env_name+'Env-v0')])
+eval_env = ObsDictWrapper(eval_env)
+
+checkpt_path = "./trained_models/"+env_name+"_sac/"
+if not os.path.exists(checkpt_path):
+    os.makedirs(checkpt_path)
+
+if train_model:
+    checkpoint_callback = CheckpointCallback(save_freq=10000,save_path=checkpt_path)
+    eval_callback = EvalCallback(eval_env,n_eval_episodes=100,eval_freq=1e4,best_model_save_path=checkpt_path+"best/",
+                            log_path=checkpt_path+"logs/",deterministic=True)
+    callback = CallbackList([checkpoint_callback,eval_callback])
+    model.learn(int(num_steps),callback=callback)
+    model.save(checkpt_path)
+else:
+    model = SAC.load(checkpt_path+"/best/best_model",env=env)
